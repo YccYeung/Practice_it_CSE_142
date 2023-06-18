f3076f9239cf1c61fe603255c9ed268b4587bc78
IGoToSchoolByBus <70379183+YccYeung@users.noreply.github.com>
2023-06-18 17:59:03 -0500

    Update and rename Mantra to Mantra.java

--- CS1_Sections/Mantra.java
+++ CS1_Sections/Mantra.java
@@ -1,10 +1,10 @@
-//Write a complete Java program in a class named Mantra that produces the following output. Remove its redundancy by adding a method.
+/*Write a complete Java program in a class named Mantra that produces the following output. Remove its redundancy by adding a method.
 
 There's one thing every coder must understand:
 The System.out.println command.
 
 There's one thing every coder must understand:
-The System.out.println command.//
+The System.out.println command.*/
 
 public class Mantra{
     
