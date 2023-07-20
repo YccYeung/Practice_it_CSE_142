6d1f62fe84aa1e3776080401472814ce5136fb97
IGoToSchoolByBus <70379183+YccYeung@users.noreply.github.com>
2023-07-20 11:48:21 -0500

    Create Mantra.java

---
+++ CS1_Sections/Section 1 (printing, methods)/Mantra.java
@@ -0,0 +1,22 @@
+public class Mantra{
+    
+    public static void main(String args[]){
+        num1();
+        num2();
+        blank();
+        num1();
+        num2();
+    }
+    
+    public static void num1(){
+        System.out.println("There's one thing every coder must understand:");
+    }
+    
+    public static void num2(){
+        System.out.println("The System.out.println command.");
+    }
+    
+    public static void blank(){
+        System.out.println("");
+    }
+}
